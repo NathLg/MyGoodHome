2b629c41e385eeda6290bb81358555986d9c877f
legall <nathanael-legall@outlook.fr>
2018-07-04 16:49:33 +0200

    Global classe et bootstrap

--- mygoodhome/src/App.vue
+++ mygoodhome/src/App.vue
@@ -1,23 +1,24 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(BootstrapVue);
+
+import globalClasses from'./global-style.scss'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
-
-<style>
+<style lang="scss" scoped>
 #app {
+
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -26,3 +27,4 @@ export default {
   margin-top: 60px;
 }
 </style>
+
