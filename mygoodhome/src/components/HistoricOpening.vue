7129513efd096d80f50de7bd51b819c2ee5d31a6
Thomas Chabot <thomas.chabot@hotmail.fr>
2018-07-10 10:11:07 +0200

    Add views

---
+++ mygoodhome/src/components/HistoricOpening.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1 class="display-3" style="font-size: 20px">Historique des ouvertures</h1>
+    <div class="list-group">
+      <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">Nom du logement</h5>
+          <small>14/08/2018</small>
+        </div>
+        <div>
+          <p class="mb-1">Heure d'ouverture</p>
+          <p class="mb-1">Coordonnées utilisateur</p>
+        </div>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">Nom du logement</h5>
+          <small class="text-muted">25/08/2018</small>
+        </div>
+        <div>
+          <p class="mb-1">Heure d'ouverture</p>
+          <p class="mb-1">Coordonnées utilisateur</p>
+        </div>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">Nom du logement</h5>
+          <small class="text-muted">11/10/2018</small>
+        </div>
+        <div>
+          <p class="mb-1">Heure d'ouverture</p>
+          <p class="mb-1">Coordonnées utilisateur</p>
+        </div>
+      </a>
+    </div>
+  </div>
+</template>
---
+++ mygoodhome/src/components/LocationDetail.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <h1 style="font-size: 20px">Informations du logement</h1>
+    <div class="form-group">
+      <label class="col-form-label">Nom</label>
+      <input class="form-control" type="text" value="">
+    </div>
+    <div class="form-group">
+      <label class="col-form-label">Adresse</label>
+      <input class="form-control" type="text" value="">
+    </div>
+    <div class="form-group">
+      <label class="col-form-label">Code postal</label>
+      <input class="form-control" type="search" value="">
+    </div>
+    <div class="form-group">
+      <label class="col-form-label">Ville</label>
+      <input class="form-control" type="search" value="">
+    </div>
+    <div class="form-group">
+      <button class="btn btn-info form-control">Enregistrer</button>
+    </div>
+  </div>
+</template>
